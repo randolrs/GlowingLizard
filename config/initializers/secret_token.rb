6d3c952d1514808375f1b345f4c58f762596a0f9
Shane Randolph <r.shane.randolph@gmail.com>
2013-05-20 19:05:08 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pizza::Application.config.secret_token = '1faf0469bd4f187bf15edee01e3877cebede748b835a4234d9a84d9205ce9b3c7b23d4740aa45a1dd5fcd3f1a2f9ce709ba3e95ef9e0dc53e96a9337a390c5dd'
